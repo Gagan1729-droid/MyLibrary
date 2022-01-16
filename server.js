05c690ea60a9468951584cff985af388a5535f98
unknown <gagan123.sp@gmail.com>
2022-01-16 16:15:03 +0530

    Authors Routes

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ if(process.env.NODE_ENV !== 'Production'){
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/author')
 const mongoose = require('mongoose')
 
 app.set('view engine', 'ejs')
@@ -13,6 +16,7 @@ app.set('views', __dirname+'/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
@@ -20,5 +24,6 @@ db.on('error', error => console.error())
 db.once('open', () => console.log("Connection to mongo successful"))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
